7347422d0c422c1934f1e4bbd96dcfb9ccfc47af
Eduardo1219 <53283218+Eduardo1219@users.noreply.github.com>
2024-08-12 13:23:53 -0300

    fix: import

--- Domain/MockData/ContactsMock/ContactsMock.cs
+++ Domain/MockData/ContactsMock/ContactsMock.cs
@@ -1,10 +1,4 @@
 ﻿using Domain.Contact.Entity;
-using SharpCompress.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.MockData.ContactsMock
 {
